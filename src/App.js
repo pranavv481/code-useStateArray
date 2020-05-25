418f6483956d7c150cbd66d6d7ccb79d5ddb38bd
pranavv481 <pranavv481@gmail.com>
2020-05-25 15:22:45 +0530

    hooks array

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import HookCounterFour from './components/HookCounterFour';
+
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HookCounterFour />
+      </div>
+    );
+  }
+}
+
+export default App;
